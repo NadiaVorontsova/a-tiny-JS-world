b369a8f3cb86d586a9faee2aafcf2697847c9d2d
NadiaVorontsova <Vorontsovvaaa@gmail.com>
2022-08-23 20:41:38 +0300

    Update index.js file

--- index.js
+++ index.js
@@ -1,31 +1,73 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
+   
+   Code repository: https://github.com/NadiaVorontsova/a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
+const dog = {
+   species: 'dog',
+   name: 'Ben',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'woof-woof'
+};
+
+const cat = {
+   species: 'cat',
+   name: 'Sarah',
+   gender: 'female',
+   legs: 4,
+   hands: 0,
+   saying: 'meow^^'
+};
+
+const man = {
+   species: 'human',
+   name: 'Yura',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'Hello everybody!',
+   friends: 'Oksana'
+};
+
+const woman = {
+   species: 'human',
+   name: 'Oksana',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: 'Hello everybody!',
+   friends: 'Yura'
+};
+
+const catWoman = {
+   species: 'cat-human',
+   name: 'Halle Berry',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: cat.saying
+};
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const inhabitancy = [dog, cat, man, woman, catWoman];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+function checkFriends(inhabitant, inProp) {
+   if(inhabitant.friends){
+      return inhabitant.friends;
+   }else{
+      return 'no friends';
+   }
+}
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+function printInhabitancy(inhabitant) {
+   return `${inhabitant.species}; ${inhabitant.name}; ${inhabitant.gender}; ${inhabitant.legs}; ${inhabitant.hands}; ${inhabitant.saying}; ${checkFriends(inhabitant)};`;
+}
 
+inhabitancy.map((inhabitant) => print(printInhabitancy(inhabitant), 'div'));
 
